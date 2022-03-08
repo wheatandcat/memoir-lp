98a7811f620644cbde57862867d86f1b2ff51f41
Yohei Iino <wheatandcat@gmail.com>
2022-03-08 21:24:09 +0900

    update: view in app (#9)

--- components/templates/Privacy/Page.tsx
+++ components/templates/Privacy/Page.tsx
@@ -1,25 +1,38 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Page = () => {
+    const router = useRouter();
+    const { app } = router.query;
+
+    let textClassName = "font-thin";
+    if (app === "true") {
+        textClassName = "";
+    }
+
     return (
-        <div className="container flex justify-center pt-10 md:pt-20 mx-auto mb-20 text-secondary-300">
+        <div
+            className={`container flex justify-center ${
+                app === "true" ? "" : "pt-10 md:pt-20"
+            } mx-auto mb-20 text-secondary-300`}
+        >
             <div className="py-8 md:py-16 px-4 md:px-20 max-w-5xl text-sm md:text-base leading-relaxed bg-white-300 md:rounded-xl">
                 <div>
-                    <h1 className="pb-5 md:pb-8 text-2xl md:text-4xl">プライバシーポリシー</h1>
-                    <div className="font-thin">
+                    {app !== "true" && <h1 className="pb-5 md:pb-8 text-2xl md:text-4xl">プライバシーポリシー</h1>}
+                    <div className={`${textClassName}`}>
                         <p>
                             ＿＿＿＿＿＿＿＿（以下、「当社」といいます。）は、本ウェブサイト上で提供するサービス（以下,「本サービス」といいます。）における、ユーザーの個人情報の取扱いについて、以下のとおりプライバシーポリシー（以下、「本ポリシー」といいます。）を定めます。
                         </p>
                     </div>
                     <h2 className="py-5 text-lg md:text-xl">第1条（個人情報）</h2>
-                    <div className="font-thin">
+                    <div className={`${textClassName}`}>
                         <p>
                             「個人情報」とは、個人情報保護法にいう「個人情報」を指すものとし、生存する個人に関する情報であって、当該情報に含まれる氏名、生年月日、住所、電話番号、連絡先その他の記述等により特定の個人を識別できる情報及び容貌、指紋、声紋にかかるデータ、及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。
                         </p>
                     </div>
 
                     <h2 className="py-5 text-lg md:text-xl">第2条（個人情報の収集方法）</h2>
-                    <div className="font-thin">
+                    <div className={`${textClassName}`}>
                         <p>
                             当社は、ユーザーが利用登録をする際に氏名、生年月日、住所、電話番号、メールアドレス、銀行口座番号、クレジットカード番号、運転免許証番号などの個人情報をお尋ねすることがあります。また、ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を,当社の提携先（情報提供元、広告主、広告配信先などを含みます。以下、｢提携先｣といいます。）などから収集することがあります。
                         </p>
@@ -27,7 +40,7 @@ const Page = () => {
 
                     <h2 className="py-5 text-lg md:text-xl">第3条（個人情報を収集・利用する目的）</h2>
                     <p>当社が個人情報を収集・利用する目的は、以下のとおりです。</p>
-                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                         <li>当社サービスの提供・運営のため</li>
                         <li>ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</li>
                         <li>
@@ -43,7 +56,7 @@ const Page = () => {
                     </ol>
 
                     <h2 className="py-5 text-lg md:text-xl">第4条（利用目的の変更）</h2>
-                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                         <li>
                             当社は、利用目的が変更前と関連性を有すると合理的に認められる場合に限り、個人情報の利用目的を変更するものとします。
                         </li>
@@ -53,10 +66,10 @@ const Page = () => {
                     </ol>
 
                     <h2 className="py-5 text-lg md:text-xl">第5条（個人情報の第三者提供）</h2>
-                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                         <li>
                             当社は、次に掲げる場合を除いて、あらかじめユーザーの同意を得ることなく、第三者に個人情報を提供することはありません。ただし、個人情報保護法その他の法令で認められる場合を除きます。
-                            <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                            <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                                 <li>
                                     人の生命、身体または財産の保護のために必要がある場合であって、本人の同意を得ることが困難であるとき
                                 </li>
@@ -68,7 +81,7 @@ const Page = () => {
                                 </li>
                                 <li>
                                     予め次の事項を告知あるいは公表し、かつ当社が個人情報保護委員会に届出をしたとき
-                                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                                         <li>利用目的に第三者への提供を含むこと</li>
                                         <li>第三者に提供されるデータの項目</li>
                                         <li>第三者への提供の手段または方法</li>
@@ -80,7 +93,7 @@ const Page = () => {
                         </li>
                         <li>
                             前項の定めにかかわらず、次に掲げる場合には、当該情報の提供先は第三者に該当しないものとします。
-                            <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                            <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                                 <li>
                                     当社が利用目的の達成に必要な範囲内において個人情報の取扱いの全部または一部を委託する場合
                                 </li>
@@ -93,10 +106,10 @@ const Page = () => {
                     </ol>
 
                     <h2 className="py-5 text-lg md:text-xl">第6条（個人情報の開示）</h2>
-                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                         <li>
                             当社は、本人から個人情報の開示を求められたときは、本人に対し、遅滞なくこれを開示します。ただし、開示することにより次のいずれかに該当する場合は、その全部または一部を開示しないこともあり、開示しない決定をした場合には、その旨を遅滞なく通知します。なお、個人情報の開示に際しては、1件あたり1、000円の手数料を申し受けます。
-                            <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                            <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                                 <li>本人または第三者の生命、身体、財産その他の権利利益を害するおそれがある場合</li>
                                 <li>当社の業務の適正な実施に著しい支障を及ぼすおそれがある場合</li>
                                 <li>その他法令に違反することとなる場合</li>
@@ -108,7 +121,7 @@ const Page = () => {
                     </ol>
 
                     <h2 className="py-5 text-lg md:text-xl">第7条（個人情報の訂正および削除）</h2>
-                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                         <li>
                             ユーザーは、当社の保有する自己の個人情報が誤った情報である場合には、当社が定める手続きにより、当社に対して個人情報の訂正、追加または削除（以下、「訂正等」といいます。）を請求することができます。
                         </li>
@@ -121,7 +134,7 @@ const Page = () => {
                     </ol>
 
                     <h2 className="py-5 text-lg md:text-xl">第8条（個人情報の利用停止等）</h2>
-                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                         <li>
                             当社は、本人から、個人情報が、利用目的の範囲を超えて取り扱われているという理由、または不正の手段により取得されたものであるという理由により、その利用の停止または消去（以下、「利用停止等」といいます。）を求められた場合には、遅滞なく必要な調査を行います。
                         </li>
@@ -137,7 +150,7 @@ const Page = () => {
                     </ol>
 
                     <h2 className="py-5 text-lg md:text-xl">第9条（プライバシーポリシーの変更）</h2>
-                    <ol className="pl-4 md:pl-10 font-thin list-decimal">
+                    <ol className={`pl-4 md:pl-10 ${textClassName} list-decimal`}>
                         <li>
                             本ポリシーの内容は、法令その他本ポリシーに別段の定めのある事項を除いて、ユーザーに通知することなく、変更することができるものとします。
                         </li>
@@ -148,9 +161,11 @@ const Page = () => {
                     <br />
                     <p className="text-right">以上</p>
                 </div>
-                <div className="pt-10 leading-normal">
-                    <Link href="/">トップに戻る</Link>
-                </div>
+                {app !== "true" && (
+                    <div className="pt-10 leading-normal">
+                        <Link href="/">トップに戻る</Link>
+                    </div>
+                )}
             </div>
         </div>
     );
