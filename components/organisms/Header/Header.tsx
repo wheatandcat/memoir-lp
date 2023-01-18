807b8aa1a17bc52d596cc660a86e927016797a47
wheatandcat <wheatandcat@gmail.com>
2023-01-19 00:06:40 +0900

    fix

--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -1,14 +1,30 @@
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { memo, useCallback } from "react";
 import { Link as Scroll } from "react-scroll";
+import useWindowSize from "hooks/useWindowSize";
 
 const KofiButton = dynamic(() => import("react-kofi-button/dist/KofiButton"), {
     ssr: false,
 });
 
 const Header = () => {
+    const { width } = useWindowSize();
+
+    const label = useCallback(() => {
+        if (width && width < 1000) {
+            return "支援する";
+        } else if (width && width < 1530) {
+            return "支援する";
+        }
+
+        return "製作者を支援する";
+    }, [width]);
+
+    const router = useRouter();
+
     return (
         <div className="container flex justify-between items-center pt-3 mx-auto">
             <div>
@@ -18,27 +34,41 @@ const Header = () => {
                     </a>
                 </Link>
             </div>
-            <div className="hidden md:flex justify-around w-4/6 lg:w-3/6 text-sm lg:text-base">
-                <div>appダウンロード</div>
-                <div className="cursor-pointer">
-                    <Scroll to="development" smooth={true} duration={600} offset={-30}>
-                        開発について
-                    </Scroll>
-                </div>
-                <div className="cursor-pointer">
-                    <Scroll to="contact" smooth={true} duration={600} offset={-30}>
-                        お問い合わせ
-                    </Scroll>
-                </div>
-                <Link href="/privacy" passHref>
-                    <a>プライバシーポリシー</a>
-                </Link>
-                <div>
-                    <KofiButton username="memoir22983" label="制作者に奢る" preset="skinny" backgroundColor="#F680B2" />
-                </div>
-            </div>
+            {router.pathname === "/" && (
+                <>
+                    <div className="hidden lg:flex justify-around items-center w-5/6 lg:w-4/6 xl:w-3/6 text-sm lg:text-base">
+                        <div />
+                        <div className="cursor-pointer">
+                            <Scroll to="development" smooth={true} duration={600} offset={-30}>
+                                開発について
+                            </Scroll>
+                        </div>
+                        <div className="cursor-pointer">
+                            <Scroll to="contact" smooth={true} duration={600} offset={-30}>
+                                お問い合わせ
+                            </Scroll>
+                        </div>
+                        <Link href="/privacy" passHref>
+                            <a>プライバシーポリシー</a>
+                        </Link>
+
+                        <div>
+                            <KofiButton
+                                username="memoir22983"
+                                label={label()}
+                                preset="thin"
+                                backgroundColor="#F680B2"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="lg:hidden">
+                        <KofiButton username="memoir22983" label="支援する" preset="thin" backgroundColor="#F680B2" />
+                    </div>
+                </>
+            )}
         </div>
     );
 };
 
-export default Header;
+export default memo(Header);
--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const removeImports = require("next-remove-imports")();
 module.exports = removeImports({
     webpack(config) {
         config.resolve.alias["@components"] = path.join(__dirname, "components");
+        config.resolve.alias["@hooks"] = path.join(__dirname, "hooks");
         config.resolve.alias["@lib"] = path.join(__dirname, "lib");
         return config;
     },
